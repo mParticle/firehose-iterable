25d0771df1de396e0ffc32a83e12c45445b54f38
Sam Dozor <sdozor@mparticle.com>
2017-10-10 15:29:59 -0400

    Update version to 1.3.1

--- iterable-extension/src/main/java/com/mparticle/ext/iterable/IterableExtension.java
+++ iterable-extension/src/main/java/com/mparticle/ext/iterable/IterableExtension.java
@@ -326,7 +326,7 @@ public class IterableExtension extends MessageProcessor {
 
     @Override
     public ModuleRegistrationResponse processRegistrationRequest(ModuleRegistrationRequest request) {
-        ModuleRegistrationResponse response = new ModuleRegistrationResponse(NAME, "1.3.0");
+        ModuleRegistrationResponse response = new ModuleRegistrationResponse(NAME, "1.3.1");
 
         Permissions permissions = new Permissions();
         permissions.setUserIdentities(
